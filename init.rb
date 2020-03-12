e2450dbc0acafbf926be5e51f8fea6a7ce6cf685
Vegaminer <vegaminer@cryptofarm.wiki>
2020-03-12 14:46:23 +0300

    Repository creation

---
+++ init.rb
@@ -0,0 +1,11 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_issues_tabulator do
+  name 'Redmine issues tabulator'
+  author 'Keks Keksov'
+  description 'Issues lits enhanced via Tabulator.js widget for Redmine'
+  version '0.0.1'
+  url 'http://github'
+  author_url 'http://github'
+end
+
